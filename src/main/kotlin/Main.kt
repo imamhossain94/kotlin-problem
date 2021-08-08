bf80e62375b07c13f849de60bb490e8decf08d54
imamhossain94 <imamagun94@gmail.com>
2021-08-08 19:32:15 +0600

    first problem solved.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,25 @@
 fun main(args: Array<String>) {
+
+    //Parsing data from a known string accurately
+
+
     val data:String = "{receivedTime=1591457696860000, category=General, title=Notice About Final Exam. Spring, 2021, url=https://www.bubt.edu.bd/home/notice_details/619}"
 
-    println(data.split(", title=")[0])
-    println(data.split(", title=")[1])
+    val firstPart = data.split(", title=")
+    val secondPart = firstPart[0].split(", category=")
 
-    println(data.split(", title=")[1].split(", url=")[0])
-    println(data.split(", title=")[1].split(", url=")[1].split("}")[0])
-//    val splitData = data.split(',').toTypedArray()
-//
-//    val receivedTime = splitData[0].split('=').toTypedArray()[1]
-//    val category = splitData[1].split('=').toTypedArray()[1]
-//    val title= splitData[2].split('=').toTypedArray()[1]
-//    val url = splitData[3].split('=').toTypedArray()[1].replace('}', ' ').trim()
-//    println(receivedTime)
-//    println(category)
-//    println(title)
-//    println(url)
+    val title = firstPart[1].split(", url=")[0]
+    val url = firstPart[1].split(", url=")[1].replace('}', ' ').trim()
+
+
+    val receivedTime = secondPart[0].split("{receivedTime=")[1]
+    val category = secondPart[1]
+
+    println(receivedTime)
+    println(category)
+    println(title)
+    println(url)
+
 }
 
 
