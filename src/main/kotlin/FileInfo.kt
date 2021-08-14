cd3e4864468750c8b0f40ccd3417c4436f68f48e
imamhossain94 <imamagun94@gmail.com>
2021-08-14 10:46:19 +0600

    feat: getting file info from remote url issue solved.

---
+++ src/main/kotlin/FileInfo.kt
@@ -0,0 +1,47 @@
+import java.net.URL
+import java.net.URLConnection
+import java.nio.file.Paths
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.io.path.extension
+import kotlin.io.path.name
+
+/*
+    Get file information from remote url
+    author: imamhossain94 (Md. Imam Hossain)
+*/
+
+
+fun main(args: Array<String>){
+
+    val url = URL("https://www.rmp-streaming.com/media/big-buck-bunny-360p.mp4")
+    val connection:URLConnection = url.openConnection()
+
+    val fileBaseName = Paths.get(url.path).fileName
+    val fileExtension = Paths.get(url.path).extension
+    val fileName = Paths.get(url.path).name
+    val fileSize = "${String.format("%.2f", connection.contentLength.toDouble()/1048000).toDouble()} MB"
+    val fileLastModifiedDate = getFileModifiedDate(connection)
+    val contentType = connection.contentType
+
+    // Only for android
+    // video thumbnail
+    //implementation 'com.github.bumptech.glide:glide:4.12.0'
+    //GlideApp.with(context).load(url).into(image_view)
+
+    println(fileBaseName)
+    println(fileExtension)
+    println(fileName)
+    println(fileSize)
+    println(fileLastModifiedDate)
+    println(contentType)
+
+
+
+}
+
+fun getFileModifiedDate(urlConnection:URLConnection):String {
+    val sdf = SimpleDateFormat("MM/dd/yyyy")
+    val netDate = Date(urlConnection.lastModified)
+    return sdf.format(netDate)
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
-fun main(args: Array<String>) {
 
-    //Parsing data from a known string accurately
+/*
+    Parsing data from a known string accurately
+    author: imamhossain94 (Md. Imam Hossain)
+*/
+
+
+fun main(args: Array<String>) {
 
 
     val data:String = "{receivedTime=1591457696860000, category=General, title=Notice About Final Exam. Spring, 2021, url=https://www.bubt.edu.bd/home/notice_details/619}"
