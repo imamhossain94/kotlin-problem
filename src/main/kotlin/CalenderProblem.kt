5b55cf8586290e58b7c65225dc1df25af953a22a
imamhossain94 <imamagun94@gmail.com>
2021-10-18 21:26:09 +0600

    feat: CalanderProblem.kt file added

--- src/main/kotlin/CalenderProblem.kt
+++ src/main/kotlin/CalenderProblem.kt
@@ -1,7 +1,21 @@
 import java.util.*
 
-
 fun main(args: Array<String>){
 
-}
+    val firingCal: Calendar = Calendar.getInstance()
+    val currentCal: Calendar = Calendar.getInstance()
 
+    //firingCal.set(Calendar.HOUR_OF_DAY, 8)
+    firingCal.set(Calendar.MINUTE, 0)
+    firingCal.set(Calendar.SECOND, 0)
+
+    if (firingCal < currentCal) {
+        firingCal.add(Calendar.DAY_OF_MONTH, 1)
+    }
+    val intendedTime: Long = firingCal.timeInMillis
+
+    println(currentCal.time)
+    println(firingCal.time)
+    
+
+}
