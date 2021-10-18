d3d05067b7a0027fbbe8a86f71d35b5a60a20fb9
imamhossain94 <imamagun94@gmail.com>
2021-10-18 22:42:05 +0600

    feat: CalanderProblem updated

--- src/main/kotlin/CalenderProblem.kt
+++ src/main/kotlin/CalenderProblem.kt
@@ -1,21 +1,75 @@
 import java.util.*
 
+
+class TimeConverter {
+
+    var timeList = emptyList<String>()
+    var timeHour = 0
+    var timeMinute = 0
+    var totalTimeInMinutes = 0
+
+    fun convert(time:String) {
+        timeList= time.split(':')
+        timeHour = timeList[0].toInt()
+        timeMinute = timeList[1].toInt()
+        totalTimeInMinutes = (timeHour*60) + timeMinute
+    }
+
+}
+
+
 fun main(args: Array<String>){
 
-    val firingCal: Calendar = Calendar.getInstance()
-    val currentCal: Calendar = Calendar.getInstance()
+    // Kotlin add extra time with current time
 
-    //firingCal.set(Calendar.HOUR_OF_DAY, 8)
-    firingCal.set(Calendar.MINUTE, 0)
-    firingCal.set(Calendar.SECOND, 0)
+    val startTime:String = "10:00"
+    val endTime:String = "11:30"
 
-    if (firingCal < currentCal) {
-        firingCal.add(Calendar.DAY_OF_MONTH, 1)
-    }
-    val intendedTime: Long = firingCal.timeInMillis
+    val startTimeConverter = TimeConverter()
+    startTimeConverter.convert(startTime)
 
-    println(currentCal.time)
-    println(firingCal.time)
-    
+    println(startTimeConverter.timeHour)
+
+
+
+
+
+
+    val startTimeList:List<String> = startTime.split(':')
+    val startTimeHour = startTimeList[0].toInt()
+    val startTimeMinute = startTimeList[1].toInt()
+    val startTimeInMinutes = (startTimeHour*60) + startTimeMinute
+
+
+    val currentCalender: Calendar = Calendar.getInstance()
+    val currentHour = currentCalender.get(Calendar.HOUR_OF_DAY)
+    val currentMinute = currentCalender.get(Calendar.MINUTE)
+    val currentTimeInMinutes = (currentHour*60) + currentMinute
+
+//    println(startTimeInMinutes)
+//    println(currentTimeInMinutes)
+//
+//    println(currentCalender.get(Calendar.MINUTE))
+
+
+//    val currentCalender: Calendar = Calendar.getInstance()
+//
+//    val firingCal: Calendar = Calendar.getInstance()
+//    firingCal.set(Calendar.HOUR_OF_DAY, 8)
+//    firingCal.set(Calendar.MINUTE, 0)
+//    firingCal.set(Calendar.SECOND, 0)
+//
+//    if (firingCal < currentCalender) {
+//        firingCal.add(Calendar.MINUTE, 90)
+//    }
+//    val intendedTime: Long = firingCal.timeInMillis
+//
+//
+//    println(currentCalender.time)
+//    currentCalender.add(Calendar.MINUTE, 90)
+//    println(currentCalender.time)
+//
+//    println(firingCal.time)
+
 
 }
